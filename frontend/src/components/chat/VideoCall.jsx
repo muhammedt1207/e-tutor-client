72cc2ed75649c4bfbe6a4bc18571bee19917f8d1
muhammedt1207 <muhammedt127@gmail.com>
2024-07-08 14:34:57 +0530

    fix:chink file size

--- frontend/src/components/chat/VideoCall.jsx
+++ frontend/src/components/chat/VideoCall.jsx
@@ -1,23 +1,20 @@
-import  { useEffect, useRef } from 'react';
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import React, { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useSocket } from '../../contexts/SocketContext';
 
 function randomID(len) {
     let result = '';
-    if (result) return result;
-    var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
-        maxPos = chars.length,
-        i;
+    const chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP';
+    const maxPos = chars.length;
     len = len || 5;
-    for (i = 0; i < len; i++) {
+    for (let i = 0; i < len; i++) {
         result += chars.charAt(Math.floor(Math.random() * maxPos));
     }
     return result;
 }
 
 export function getUrlParams(url = window.location.href) {
-    let urlStr = url.split('?')[1];
+    const urlStr = url.split('?')[1];
     return new URLSearchParams(urlStr);
 }
 
@@ -25,48 +22,54 @@ const VideoCall = () => {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const id = queryParams.get('id');
-    const senderId=queryParams.get('senderId')
+    const senderId = queryParams.get('senderId');
     const { socket } = useSocket();
     const roomID = getUrlParams().get('roomID') || randomID(5);
     const meetingContainerRef = useRef(null);
 
     useEffect(() => {
+        let zp;
         if (meetingContainerRef.current) {
-            myMeeting(meetingContainerRef.current);
+            loadMeeting(meetingContainerRef.current);
         }
-    
-        async function myMeeting(element) {
+
+        async function loadMeeting(element) {
+            const { ZegoUIKitPrebuilt } = await import('@zegocloud/zego-uikit-prebuilt');
+
             const appID = 1641629677;
-            const serverSecret = "02fa6ccc8f061c489220e8c28f649c88";
+            const serverSecret = '02fa6ccc8f061c489220e8c28f649c88';
             const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, randomID(5), randomID(5));
-    
-            const zp = ZegoUIKitPrebuilt.create(kitToken);
-    
+
+            zp = ZegoUIKitPrebuilt.create(kitToken);
+
             zp.joinRoom({
                 container: element,
                 sharedLinks: [
                     {
                         name: 'Copy link',
-                        url: window.location.protocol + '//' + window.location.host + window.location.pathname + '?roomID=' + roomID,
+                        url: `${window.location.protocol}//${window.location.host}${window.location.pathname}?roomID=${roomID}`,
                     },
                 ],
                 scenario: {
                     mode: ZegoUIKitPrebuilt.OneONoneCall,
                 },
             });
-    
+
             const data = {
-                id:id,
-                senderId:senderId,
-                link: window.location.protocol + '//' + window.location.host + window.location.pathname + '?roomID=' + roomID,
+                id: id,
+                senderId: senderId,
+                link: `${window.location.protocol}//${window.location.host}${window.location.pathname}?roomID=${roomID}`,
             };
-    
-            
-            socket?.emit("videoCall", data);
+
+            socket?.emit('videoCall', data);
         }
-    }, [roomID, socket, id]);
-    
-    
+
+        return () => {
+            if (zp) {
+                zp.leaveRoom();
+            }
+        };
+    }, [roomID, socket, id, senderId]);
 
     return (
         <div
