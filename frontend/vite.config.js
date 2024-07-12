87de4e45a39034e0acbbe15527c1b1da0884d298
muhammedt1207 <muhammedt127@gmail.com>
2024-07-12 18:23:20 +0530

    refactor:vite-config updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,18 +17,6 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: 5000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('@zegocloud')) {
-              return 'zegocloud';
-            }
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
   },
   server: {
     host: true,
